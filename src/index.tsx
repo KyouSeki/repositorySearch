8c9a09b9e4df6f422422f614244fd03373cb16d2
KyouSeki <chenjingxi@ktwlrj.com>
2024-06-28 17:14:12 +0800

    降级到react@17.0.2, 解决缓存器的bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import App from './App'
-import { AliveScope } from 'react-activation'
+import React from "react";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App"
+import { AliveScope } from "react-activation"
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
   <AliveScope>
-            <App />
-          </AliveScope>
-);
+    <App />
+  </AliveScope>,
+  document.getElementById('root')
+)
 
 reportWebVitals();
